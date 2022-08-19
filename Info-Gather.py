60d8a77ce9b1c6319f3ab64fce74caa6b961b985
waefedfg <98655347+waefedfg@users.noreply.github.com>
2022-08-19 12:42:41 -0400

    Update Info-Gather.py

--- Info-Gather.py
+++ Info-Gather.py
@@ -2,12 +2,12 @@ import os
 import platform
 import sys
 import getpass
-
+import socket
 
 print('----------------------------------------------------')
 print('Os =', platform.system())
 print('Files =', os.listdir())
 print('Name of Current User =', getpass.getuser())
 print('System version =', sys.version)
-print('Your location is', os.getcwd())
-
+print('Your directory path is', os.getcwd())
+print('Computer Host =', socket.gethostname())
